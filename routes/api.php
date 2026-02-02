426c409ab16e653d0c6c0a15759ec7f9167ae544
Ali Shaalan <shaalanali20@gmail.com>
2026-02-02 16:52:28 +0200

    user section routes,created controller4

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\UserAuthController;
 use App\Http\Controllers\Auth\DoctorAuthController;
 use App\Http\Controllers\Auth\AdminAuthController;
+use App\Http\Controllers\User\ProfileController;
+use App\Http\Controllers\User\DoctorController;
+use App\Http\Controllers\User\AppointmentController;
+
 
 Route::prefix('auth')->group(function () {
 
@@ -18,4 +22,22 @@ Route::prefix('auth')->group(function () {
     // admin
     Route::post('/admin/login', [AdminAuthController::class, 'login']);
     Route::post('/admin/register', [AdminAuthController::class, 'register']);
+});
+
+
+Route::middleware(['auth:sanctum','role:user'])->prefix('user')->group(function () {
+
+    // Profile
+    Route::get('/profile', [ProfileController::class, 'show']);
+    Route::put('/profile', [ProfileController::class, 'update']);
+
+    // Doctors
+    Route::get('/doctors', [DoctorController::class, 'index']);
+    Route::get('/doctors/{doctor}', [DoctorController::class, 'show']);
+    Route::get('/doctors/search', [DoctorController::class, 'search']);
+    Route::get('/doctors/{doctor}/availability', [DoctorController::class, 'availability']);
+
+    // Appointments
+    Route::post('/appointments', [AppointmentController::class, 'store']); 
+    Route::get('/appointments', [AppointmentController::class, 'index']);
 });
