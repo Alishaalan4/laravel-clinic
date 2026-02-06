066f8dc49ad008fbef95c31cef7d54a8840cfd0b
Ali Shaalan <shaalanali20@gmail.com>
2026-02-06 16:12:26 +0200

    admin/appointment controller4

--- app/Http/Controllers/Admin/AppointmentController.php
+++ app/Http/Controllers/Admin/AppointmentController.php
@@ -4,8 +4,49 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Appointment;
+use App\Models\User;
+use App\Models\Doctor;
+use Carbon\Carbon;
 
 class AppointmentController extends Controller
 {
-    //
+        public function index()
+    {
+        return response()->json(
+            Appointment::with(['doctor', 'user'])->get()
+        );
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'user_id'          => 'required|exists:users,id',
+            'doctor_id'        => 'required|exists:doctors,id',
+            'appointment_date' => 'required|date',
+            'appointment_time' => 'required|date_format:H:i',
+        ]);
+
+        $start = Carbon::createFromFormat('H:i', $data['appointment_time']);
+        $end   = $start->copy()->addMinutes(30);
+
+        $appointment = Appointment::create([
+            'user_id'          => $data['user_id'],
+            'doctor_id'        => $data['doctor_id'],
+            'appointment_date' => $data['appointment_date'],
+            'appointment_time' => $start->format('H:i'),
+            'end_time'         => $end->format('H:i'),
+            'status'           => 'booked',
+        ]);
+
+        return response()->json([
+            'message' => 'Appointment created by admin',
+            'appointment' => $appointment
+        ], 201);
+    }
+    public function show($id)
+    {
+        $appointment = Appointment::find($id);
+        return response()->json($appointment);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -84,7 +84,7 @@ Route::middleware(['auth:sanctum','role:admin'])->prefix('admin')->group(functio
 
     // Users
     Route::get('/users', [UserController::class, 'index']);
-    Route::get('/users{user}', [UserController::class,'show']);
+    Route::get('/users/{user}', [UserController::class,'show']);
     Route::post('/users/{user}/changePassword', [UserController::class,'changePassword']);
     Route::delete('/users/{user}', [UserController::class, 'destroy']);
 
